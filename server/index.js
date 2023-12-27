db785dd5e1a8fee641be9985ba46a27c07dbaddd
Ashru-amj <96196448+Ashru-amj@users.noreply.github.com>
2023-12-27 07:46:40 +0530

    change

--- server/index.js
+++ server/index.js
@@ -1,45 +1,54 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { fileURLToPath, URL } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(cookieParser());
-mongoose.connect(process.env.MONGO,{
+
+mongoose
+  .connect(process.env.MONGO, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     writeConcern: {
-        w: 'majority',  // Note: Without the semicolon
+      w: 'majority',
     },
-})
+  })
   .then(() => console.log('mongoose connected'))
   .catch((error) => console.error(error));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use('/api/user', userRoutes);
-app.use('/api/auth',authRoutes)
+app.use('/api/auth', authRoutes);
 
+// Use path.resolve to get the absolute path of 'build' directory
+app.use(express.static(new URL('build', import.meta.url).pathname));
 
+app.get('/', function (req, res) {
+  res.sendFile(new URL('build/index.html', import.meta.url).pathname);
+});
 
-app.use((err, req,res,next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     message,
     statusCode,
-
-  })
-})
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
-
-
